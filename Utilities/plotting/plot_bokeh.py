af002b280277e2609ba1ab81bb3a9af8fa3563d9
Brian Schaefer <physinet@gmail.com>
2016-08-31 01:50:20 -0400

    Added line plotting
    Still need to deal with line and marker styling

--- Utilities/plotting/plot_bokeh.py
+++ Utilities/plotting/plot_bokeh.py
@@ -29,10 +29,10 @@ def figure(title=None, xlabel=None, ylabel=None):
     '''
     ## Set up the figure
     import bokeh.plotting as bplt
-    fig = bplt.figure(plot_width=400,
-                plot_height=400,
-                title=None,
-                tools='pan,reset,save,box_zoom,wheel_zoom,resize'
+    fig = bplt.figure(plot_width = 600,
+                plot_height = 600,
+                tools = 'pan,reset,save,box_zoom,wheel_zoom,resize',
+                title = title,
                 )
 
     ## Add data hover tool
@@ -47,13 +47,52 @@ def figure(title=None, xlabel=None, ylabel=None):
     )
     fig.add_tools(hover)
 
-    ## Set up title and axis labels
+    ## Set up title and axis text
+    # title
+    fig.title.text_font_size = '30pt'
+    # X
     fig.xaxis.axis_label = xlabel
+    fig.xaxis.axis_label_text_font_size = '20pt'
+    fig.xaxis.major_label_text_font_size = '12pt'
+    # Y
     fig.yaxis.axis_label = ylabel
-    fig.title.text = title
+    fig.yaxis.axis_label_text_font_size = '20pt'
+    fig.yaxis.major_label_text_font_size = '12pt'
+
+
 
     return fig
 
+
+def line(fig, x, y, line_handle=None, color='black', linestyle='-', markerstyle=None, name=None):
+    '''
+    Adds a line to a bokeh plot.
+    Must specify figure and x and y data.
+    Note: once figure is shown, will not add any new lines.
+    Optional arguments:
+    - line_handle: Pass in the handle for a line to update that line rather than create a new one.
+    - color: any CSS colorname (http://www.crockford.com/wrrrld/color.html) or Hex RGB
+    - linestyle: Mimicking matplotlib, can specify linestyle.
+        Line types: - (solid), -- (dashed), : (dotted), .- (dotdash), -. (dashdot)
+    - markerstyle: Mimicking matplotlib, can specify markerstyle.
+        Marker types: o (circle), s (square), v (inverted_triangle), ^ (triangle), x (x), + (cross), * (asterisk)
+    - name: label for this line
+    '''
+
+    # TO DO: implement linestyle, markerstyle
+
+
+    if line_handle: # if updating an existing line
+        line_handle.data_source.data['x'] = x
+        line_handle.data_source.data['y'] = y
+
+    else: # create a new line
+        from bokeh.models import ColumnDataSource
+        source = ColumnDataSource(data=dict(x=x,y=y, name=[name]*len(x))) # an array of "name"s will have "name" show up on hover tooltip
+        line_handle = fig.line(x, y, color=color, line_width=2, source=source, name=name)
+
+    update() # refresh plots in the notebook
+    return line_handle
 
 def plot_html(fig):
     '''
@@ -79,6 +118,7 @@ def plot_html(fig):
     fig.name = figfile
 
 
+
 def show(fig):
     '''
     Show a bokeh plot using the normal show function.
@@ -101,4 +141,7 @@ def update():
     '''
     Replots a bokeh figure if it has been changed.
     '''
-    bokeh.io.push_notebook(handle=NB_HANDLE[get_nb_kernel()])
+    try:
+        bokeh.io.push_notebook(handle=NB_HANDLE[get_nb_kernel()])
+    except:
+        pass # This code reached if we haven't run show() yet!
