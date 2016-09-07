979ab52047b0d51238f360dc3c17e5da99ee4504
Brian Schaefer <physinet@gmail.com>
2016-09-07 12:12:52 -0400

    Navigation cleanup

---
+++ Procedures/navigation.py
@@ -0,0 +1,14 @@
+from ..Utilities import dummy
+from ..Instruments import piezos, attocube
+
+class Goto():
+    '''
+    Will write this once we have a good sense of directions with attocubes and piezos.
+    '''
+    def __init__(self, instruments):
+        if instruments is not None:
+            self.piezos = instruments['piezos']
+            self.atto = instruments['atto']
+        else:
+            self.piezos = dummy.Dummy(piezos.Piezos)
+            self.atto = dummy.Dummy(attocube.Attocube)
--- Utilities/__init__.py
+++ Utilities/__init__.py
@@ -1,3 +1,1 @@
-from .plotting import *
-__all__ = ['dummy', 'make_instruments', 'utilities']
 __all__ = ['dummy', 'make_instruments', 'plotting', 'utilities']
