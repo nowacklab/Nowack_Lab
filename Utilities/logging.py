4fc0ccecf096b9010f575159f66428245ea39665
nowacklab-hemlock <nowacklab@gmail.com>
2016-09-22 11:25:39 -0400

    Logger was logging months instead of minutes

--- Utilities/logging.py
+++ Utilities/logging.py
@@ -12,7 +12,7 @@ def log_console(msg):
     '''
     Logs a message to the console with timestamp.
     '''
-    now = datetime.datetime.now().strftime('%H:%m:%S')
+    now = datetime.datetime.now().strftime('%H:%M:%S')
     full_msg = '[SQUIDLogger %s] %s\n' %(now, msg)
     os.write(1, bytes(full_msg, encoding='utf-8'))
 
