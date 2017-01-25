b4e8fb9404686f36614d46cafbaeb7032adda717
Guen P <guen@nbi.dk>
2017-01-25 10:01:58 -0500

    Revert "Revert "Edits for PPMS computer""
    This reverts commit 612929e16197e92653fc9878ba3c92c992f743d4.

--- runme.py
+++ runme.py
@@ -20,10 +20,10 @@ home = os.path.expanduser('~')
 site_packages = os.path.dirname(os.path.abspath(os.path.join(matplotlib.__file__, '..')))
 
 ## Add utility to save jupyter notebook to HTML
-package_path = os.path.join(os.getcwd(),'Utilities','jupyter_notebook_config.py')
-jupyter_path = os.path.join(home, '.jupyter', 'jupyter_notebook_config.py')
+# package_path = os.path.join(os.getcwd(),'Utilities','jupyter_notebook_config.py')
+# jupyter_path = os.path.join(home, '.jupyter', 'jupyter_notebook_config.py')
 
-shutil.copyfile(package_path, jupyter_path)
+# shutil.copyfile(package_path, jupyter_path)
 
 ## Add github.pth file to enable "from Nowack_Lab import *"
 anaconda_path = os.path.join(site_packages,'github.pth')
@@ -32,21 +32,21 @@ with open(anaconda_path, 'w') as f:
     f.write(os.path.dirname(os.getcwd())) # Writes Github directory name
 
 ## Add custom.css file to add padding to bottom of the notebook
-package_path = os.path.join(os.getcwd(),'Utilities','custom.css')
-notebook_path = os.path.join(site_packages, 'notebook', 'static', 'custom', 'custom.css')
+# package_path = os.path.join(os.getcwd(),'Utilities','custom.css')
+# notebook_path = os.path.join(site_packages, 'notebook', 'static', 'custom', 'custom.css')
 
-shutil.copyfile(package_path, notebook_path)
+# shutil.copyfile(package_path, notebook_path)
 
 ## Add custom.js file to disable autosave
-package_path = os.path.join(os.getcwd(),'Utilities','custom.js')
-notebook_path = os.path.join(site_packages, 'notebook', 'static', 'custom', 'custom.js')
+# package_path = os.path.join(os.getcwd(),'Utilities','custom.js')
+# notebook_path = os.path.join(site_packages, 'notebook', 'static', 'custom', 'custom.js')
 
-shutil.copyfile(package_path, notebook_path)
+# shutil.copyfile(package_path, notebook_path)
 
 ## Clean up startup if exists.
-ipython_path = os.path.join(home, '.ipython', 'profile_default', 'startup', 'startup.py')
+# ipython_path = os.path.join(home, '.ipython', 'profile_default', 'startup', 'startup.py')
 
-try:
-    os.remove(ipython_path)
-except:
-    pass
+# try:
+#     os.remove(ipython_path)
+# except:
+#     pass
