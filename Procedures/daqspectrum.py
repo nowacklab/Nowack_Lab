396704a4c6ccd1d30e6f8d3fa24a2d55259d237d
nowacklab-hemlock <nowacklab@gmail.com>
2017-05-11 16:21:34 -0400

    Add conversion factor to noise spectrum
    New class SQUIDSpectrum takes in squidarray to determine conversion
    factor.

--- Procedures/daqspectrum.py
+++ Procedures/daqspectrum.py
@@ -7,6 +7,7 @@ import re
 from ..Instruments import nidaq, preamp
 from ..Utilities.save import Measurement
 from ..Utilities.utilities import AttrDict
+from ..Utilities import conversions
 
 class DaqSpectrum(Measurement):
     _chan_labels = ['dc'] # DAQ channel labels expected by this class
@@ -16,10 +17,11 @@ class DaqSpectrum(Measurement):
     V = 1
     t = 1
     psdAve = 1
+    units = 'V'
+    conversion = 1
 
     def __init__(self, instruments={}, measure_time=0.5, measure_freq=256000, averages=30):
         super().__init__()
-
 
         self._load_instruments(instruments)
 
@@ -57,41 +59,42 @@ class DaqSpectrum(Measurement):
 
     def plot(self):
         super().plot()
-        self.ax['loglog'].loglog(self.f, self.psdAve)
-        self.ax['semilog'].semilogy(self.f, self.psdAve)
+        self.ax['loglog'].loglog(self.f, self.psdAve*self.conversion)
+        self.ax['semilog'].semilogy(self.f, self.psdAve*self.conversion)
 
-
-    def plotLog(self, fname, calibration=None):
-        '''
-        Generate a log-log plot of spectrum. If there is a known calibration
-        between RMS voltage noise and flux noise, the plot is generated in units
-        of flux quanta. Use daqspectrum.load to get all the data before calling
-        plotLog.
-
-        calibration should be in units of Phi_o/V
-
-        THIS MAY NOT WORK ANYMORE
-        '''
-        self.load(fname)
-        fig, ax = plt.subplots(figsize=(5,5))
-        #if a calibration is provided, rescale the y axis
-        #also label with appropriate units
-        if calibration:
-            self.psdAve = self.psdAve / calibration
-            ax.set_ylabel(r'Flux Noise ($\mathrm{\Phi_o/\sqrt{Hz}}$)')
-        else:
-            ax.set_ylabel(r'RMS Voltage Noise ($V_{rms}/\sqrt{Hz}$)')
-        ax.loglog(self.f, self.psdAve)
-        ax.set_xlabel(r'Frequency (Hz)')
-        ax.set_xlim([0, 200000])
-        #annotate the plot with the timestamp
-        ax.annotate(self.timestamp, xy=(0.005, 1.02), xycoords='axes fraction', fontsize=10, family='monospace')
-        #generate the name of the plot from the initial filename
-        figPathPng = os.path.splitext(fname)[0] + '.png'
-        figPathPdf = os.path.splitext(fname)[0] + '.pdf'
-        plt.savefig(figPathPng, dpi=400)
-        plt.savefig(figPathPdf)
-        return figPathPng
+        ## UNUSED
+    #
+    # def plotLog(self, fname, calibration=None):
+    #     '''
+    #     Generate a log-log plot of spectrum. If there is a known calibration
+    #     between RMS voltage noise and flux noise, the plot is generated in units
+    #     of flux quanta. Use daqspectrum.load to get all the data before calling
+    #     plotLog.
+    #
+    #     calibration should be in units of Phi_o/V
+    #
+    #     THIS MAY NOT WORK ANYMORE
+    #     '''
+    #     self.load(fname)
+    #     fig, ax = plt.subplots(figsize=(5,5))
+    #     #if a calibration is provided, rescale the y axis
+    #     #also label with appropriate units
+    #     if calibration:
+    #         self.psdAve = self.psdAve / calibration
+    #         ax.set_ylabel(r'Flux Noise ($\mathrm{\Phi_o/\sqrt{Hz}}$)')
+    #     else:
+    #         ax.set_ylabel(r'RMS Voltage Noise ($V_{rms}/\sqrt{Hz}$)')
+    #     ax.loglog(self.f, self.psdAve)
+    #     ax.set_xlabel(r'Frequency (Hz)')
+    #     ax.set_xlim([0, 200000])
+    #     #annotate the plot with the timestamp
+    #     ax.annotate(self.timestamp, xy=(0.005, 1.02), xycoords='axes fraction', fontsize=10, family='monospace')
+    #     #generate the name of the plot from the initial filename
+    #     figPathPng = os.path.splitext(fname)[0] + '.png'
+    #     figPathPdf = os.path.splitext(fname)[0] + '.pdf'
+    #     plt.savefig(figPathPng, dpi=400)
+    #     plt.savefig(figPathPdf)
+    #     return figPathPng
 
 
     def setup_plots(self):
@@ -102,7 +105,7 @@ class DaqSpectrum(Measurement):
 
         for ax in self.ax.values():
             ax.set_xlabel('Frequency (Hz)')
-            ax.set_ylabel(r'Power Spectral Density ($\mathrm{V/\sqrt{Hz}}$)')
+            ax.set_ylabel(r'Power Spectral Density ($\mathrm{%s/\sqrt{Hz}}$)' %self.units)
             #apply a timestamp to the plot
             ax.annotate(self.timestamp, xy=(0.02,.98), xycoords='axes fraction',
                 fontsize=10, ha='left', va = 'top', family='monospace'
@@ -118,3 +121,12 @@ class DaqSpectrum(Measurement):
         self.preamp.dc_coupling()
         self.preamp.diff_input(False)
         self.preamp.filter_mode('low',12)
+
+class SQUIDSpectrum(DaqSpectrum):
+    _chan_labels = ['dc'] # DAQ channel labels expected by this class
+    instrument_list = ['daq', 'preamp', 'squidarray']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.units = '\phi_0'
+        self.conversion = conversions.Vsquid_to_phi0[self.squidarray.sensitivity]
