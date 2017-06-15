2e2157c646d3453266660ac7f39a0fc0584e9221
Brian Schaefer <physinet@gmail.com>
2017-06-15 15:46:51 -0400

    Add utility to convert h5 data to txt
    Formatting may need some work, but it is a good first step.

---
+++ Utilities/data_to_txt.py
@@ -0,0 +1,44 @@
+'''
+Convert an HDF5 file to plain txt. Start of a new dataset is marked by #.
+From commandline, run "python data_to_txt.py <filename>"
+"##" Marks the beginning of a dictionary or object
+"#" Marks the beginning of a numpy array
+"@@" Marks the end of a dictionary or object
+"@" Marks the end of a numpy array
+'''
+import sys, os
+import numpy as np
+
+from Nowack_Lab.Utilities.save import Measurement
+from Nowack_Lab.Utilities import utilities
+
+def data_to_txt(filename):
+    '''
+    filename: path to .h5 or .json file from a Measurement
+    '''
+
+    m = Measurement.load(filename)
+    filename = os.path.splitext(filename)[0]
+    with open(filename+'.txt', 'w') as f:
+        # Walk through the dictionary
+        def walk(d):
+            for key, value in d.items():
+                # If a numpy array is found
+                if type(value) is np.ndarray:
+                    f.write('# %s\n' %key)
+                    f.write(str(value.tolist()))
+                    f.write('\n@ \n')
+                elif 'dict' in utilities.get_superclasses(value):
+                    f.write('## %s\n' %key)
+                    walk(value) # walk through the dictionary
+                    f.write('@@\n')
+                # If the there is some other object
+                elif hasattr(value, '__dict__'):
+                    if 'Measurement' in utilities.get_superclasses(value): # restrict saving Measurements.
+                        f.write('## %s\n' %key)
+                        walk(value.__dict__) # walk through the object dictionary
+                        f.write('@@\n')
+        walk(m.__dict__)
+
+if __name__ == '__main__':
+    data_to_txt(sys.argv[1])
--- Utilities/save.py
+++ Utilities/save.py
@@ -344,13 +344,7 @@ class Measurement:
             os.path.join(get_local_data_path(), get_todays_data_dir(), filename)
 
         # Remove file extensions
-        # This is done somewhat manually in case filename has periods in it for some reason.
-        if filename[-5:] == '.json': # ends in .json
-            filename = filename[:-5] # strip extension
-        elif filename[-3:] == '.h5': # ends in .h5
-            filename = filename[:-3] # strip extension
-        elif filename[-4:] == '.pdf': # ends in .pdf
-            filename = filename[:-4] # strip extension
+        filename = os.path.splitext(filename)[0]
 
         obj = Measurement._load_json(filename+'.json', unwanted_keys)
         obj._load_hdf5(filename+'.h5')
