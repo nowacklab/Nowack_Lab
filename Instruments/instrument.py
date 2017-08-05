9c4698d6a984c19b562283708ea5e649de3ce2f7
abj46 <abj46@cornell.edu>
2017-08-05 12:29:44 -0400

    Updated to include Brian's visainstrument

--- Instruments/instrument.py
+++ Instruments/instrument.py
@@ -1,6 +1,8 @@
 '''
-For the future? Instrument base class that all instruments belong to.
+Instrument base classes.
 '''
+import visa
+
 class Instrument:
     _label = 'instrument'
 
@@ -15,3 +17,61 @@ class Instrument:
     	an unwanted change to the physical instrument.
     	'''
     	pass
+
+class VISAInstrument(Instrument):
+    _label = 'VISAinstrument'
+
+    def __del__(self):
+        '''
+        Destroy the object and close the visa handle
+        '''
+        self.close()
+
+    def _init_visa(self, resource, termination='\n'):
+        r'''
+        Initialize the VISA connection.
+        Pass in the resource name. This can be:
+        - GPIB Address
+            GPIB::##::INSTR
+        - TCPIP Socket
+            TCPIP::host address::port::SOCKET
+        - COM port
+            COM#
+        - Or many others...
+            See https://pyvisa.readthedocs.io/en/stable/names.html
+        termination: e.g. \r\n: read termination.
+        '''
+        self._visa_handle = visa.ResourceManager().open_resource(resource)
+        self._visa_handle.read_termination = termination
+
+    def ask(self, cmd, timeout=3000):
+        '''
+        Write and read combined operation.
+        Default timeout 3000 ms. None for infinite timeout
+        Strip terminating characters from the response.
+        '''
+        self._visa_handle.timeout = timeout
+        data = self._visa_handle.ask(cmd)
+        return data.rstrip()
+
+    def close(self):
+        '''
+        Close the visa connection.
+        '''
+        if hasattr(self, '_visa_handle'):
+            self._visa_handle.close()
+            del(self._visa_handle)
+
+    def read(self):
+        '''
+        Read from VISA.
+        Strip terminating characters from the response.
+        '''
+        data = self._visa_handle.read()
+        return data.rstrip()
+
+    def write(self, cmd):
+        '''
+        Write to VISA.
+        '''
+        self._visa_handle.write(cmd)
