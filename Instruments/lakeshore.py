4d18cd4e95427e125c996ec75aeb01392592dd8b
David Low: Manchot <dhl88@cornell.edu>
2017-08-11 17:22:57 -0400

    Added changes to lakeshore and magnet from Measurement branch

--- Instruments/lakeshore.py
+++ Instruments/lakeshore.py
@@ -1,50 +1,159 @@
 from .instrument import VISAInstrument
+import numpy as np
 
-class Lakeshore372(VISAInstrument):
+class LakeshoreChannel(VISAInstrument):
     '''
-    Driver to communicate with LakeShore Model 372 AC Resistance Bridge & 
-    Temperature Controller for the BlueFors dilution refrigerator
+    Channel objected for the LakeShore Model 372 AC Resistance bridge.
+    The LakeShore372 class defined below contains up to 8 channel objects
+    representing each of the input channels on the instrument.
     '''
-    _label = 'Lakeshore Model 372'
+    _label = 'LakeshoreChannel'
+    _num = 0
     _strip = '\r'
-    _P = {}
-    _R = {}
-    _status = {}
-    _T = {}
+    _insets = ['enabled', 'dwell', 'pause', 'curve_num', 'temp_coef']
 
-    def __init__(self, host = '192.168.100.143', port=7777):
-        self._init_visa('TCPIP::%s::%i::SOCKET' %(host, port), termination='\n')
-        self._channel_names = {
-            1: '50K',
-            2: '4K',
-            3: 'Magnet',
-            5: 'Still',
-            6: 'MXC',
-            7: 'User1',
-            8: 'User2'
+    def __init__(self, visa_handle, num, label):
+        '''
+        Arguments:
+            visa_handle: VISA handle created for the LakeShore372
+            num: Channel number.
+            label: Channel label.
+        '''
+        self._visa_handle = visa_handle
+        self._num = num
+        self._label = label
+
+        # Properties for all the channel settings
+        for i, var in enumerate(self._insets):
+            setattr(LakeshoreChannel, var, property(
+                fget = eval('lambda self: self._get_inset()[%i]' %i),
+                fset = eval('lambda self, value: self._set_inset(%s = value)'
+                %var)
+            ))
+
+
+    def __getstate__(self):
+        self._save_dict = {
+            'Channel number': self._num,
+            'Channel label': self._label,
+            'Channel enabled?': self.enabled,
+            'Channel being scanned?': self.scanned,
+            'power': self.P,
+            'resistance': self.R,
+            'temperature': self.T,
+            'status': self.status,
+            'dwell time': self.dwell,
+            'pause': self.pause,
+            'curve number': self.curve_num,
+            'temperature coefficient 1pos, 2neg': self.temp_coef,
         }
+        return self._save_dict
+
+
+    def __setstate__(self, state):
+        state['_num'] = state.pop('Channel number')
+        state['_label'] = state.pop('Channel label')
+        state['_enabled'] = state.pop('Channel enabled?')
+        state['_scanned'] = state.pop('Channel being scanned?')
+        state['_P'] = state.pop('power')
+        state['_R'] = state.pop('resistance')
+        state['_T'] = state.pop('temperature')
+        state['_status'] = state.pop('status')
+        state['_dwell'] = state.pop('dwell time')
+        state['_pause'] = state.pop('pause')
+        state['_curve_num'] = state.pop('curve number')
+        state['_temp_coef'] = state.pop('temperature coefficient 1pos, 2neg')
+
+        self.__dict__.update(state)
+
+
+    def _get_inset(self):
+        '''
+        Returns all the parameters returned by the INSET? query.
+        These are channel settings in an array:
+            [enabled/disabled, dwell, pause, curve number, tempco]
+        '''
+        inset = self.ask('INSET? %i' %self._num)
+        inset = [int(x) for x in inset.split(',')]
+        inset[0] = bool(inset[0]) # make enabled True/False
+        for i, var in enumerate(self._insets):
+            setattr(self, '_'+var, inset[i])
+        return inset
+
+
+    def _set_inset(self, enabled = None, dwell = None, pause = None,
+                    curve_num = None, temp_coef = None
+        ):
+        '''
+        Set any number of channel settings uing the INSET command.
+        For each keyword argument, None indicates not to change that setting.
+
+        Parameters:
+            enabled (bool): enable or disable the channel
+            dwell (float): Dwell time (1-200 s)
+            pause (float): Change pause time (3-200 s)
+            curve_num (int 0-59): Which curve the channel uses. 0 = no curve.
+            temp_coef (int): Temp. coefficient used if no curve; 1 = -, 2 = +
+        '''
+        inset = self._get_inset()
+        args = [enabled, dwell, pause, curve_num, temp_coef]
+
+        for i, arg in enumerate(args):
+            if arg is None:
+                args[i] = inset[i]
+
+        cmd = 'INSET %i,%i,%i,%i,%i,%i' %(self._num, *args)
+        self.write(cmd)
+
 
     @property
     def P(self):
         '''
-        Get the power (W) of input channels as a dictionary.
+        Get the power (W) of this input channel.
         '''
-        self._P = self._loop_over_chans('RDGPWR?', float)# "ReaDinG PoWeR"
+        if self.status == 'OK':
+            self._P = float(self.ask('RDGPWR? %i' %self._num))
+        else:
+            self._P = np.nan
         return self._P
-
 
     @property
     def R(self):
         '''
-        Get the resistance (Ohm) of input channels as a dictionary.
+        Get the resistance (R) of this input channel.
         '''
-        self._R = self._loop_over_chans('RDGR?', float)# "ReaDinG Resistance"
+        if self.status == 'OK':
+            self._R = float(self.ask('RDGR? %i' %self._num))
+        else:
+            self._R = np.nan
         return self._R
+
+    @property
+    def scanned(self):
+        '''
+        Is this particular channel being scanned?
+        Returns True/False
+        '''
+        # SCAN? returns ##,#. The first number is channel being scanned
+        scan = self.ask('SCAN?')
+        scan = int(scan.split(',')[0])
+        self._scanned = (scan == self._num) # True or False
+        return self._scanned
+
+    def scan(self, autoscan=True):
+        '''
+        Start scanning this channel.
+
+        autoscan: whether to automatically loop through channels
+        '''
+        self.write('SCAN %i,%i' %(self._num, autoscan))
 
     @property
     def status(self):
         '''
-        Get the status of all input channels.
+        Get the status the input channel.
+        Status messages are standard to LakeShore, except "OK" means there are
+        no status messages.
         '''
         msgs = [
             'T. UNDER',
@@ -56,18 +165,16 @@ class Lakeshore372(VISAInstrument):
             'VCM OVL',
             'CS OVL'
         ]
+        b = int(self.ask('RDGST? %i' %self._num)) # "ReaDinG STatus"
 
-        b = self._loop_over_chans('RDGST?', int) # "ReaDinG STatus"
-        # Loop over channels and add status messages according to the status byte.
-        for chan in self._channel_names.keys():
-            status_message = ''
-            binlist = [int(x) for x in '{:08b}'.format(b[chan])] # to list of 1s and 0s
-            for i, bit in enumerate(binlist):
-                if bit:
-                    status_message += msgs[i] + '; ' # add status message
-            if status_message == '':
-                status_message = 'OK'
-            self._status[chan] = status_message.rstrip('; ') # remove if unnecessary
+        status_message = ''
+        binlist = [int(x) for x in '{:08b}'.format(b)] # to list of 1s and 0s
+        for i, bit in enumerate(binlist):
+            if bit:
+                status_message += msgs[i] + '; ' # add status message
+        if status_message == '':
+            status_message = 'OK'
+        self._status = status_message.rstrip('; ') # remove if unnecessary
         return self._status
 
     @property
@@ -75,123 +182,101 @@ class Lakeshore372(VISAInstrument):
         '''
         Get the temperature (K) reading of input channels as a dictionary.
         '''
-        self._T = self._loop_over_chans('RDGK?', float)# "ReaDinG Kelvin"
+        if self.status == 'OK':
+            self._T = float(self.ask('RDGK? %i' %self._num))
+        else:
+            self._T = np.nan
         return self._T
 
-    def getchsettings(self):
-        '''
-        Gets all channel settings
-        
-        returns dictionary, key is channel number, value is 
-        array as in get_channel
-        '''
-        cmd = "INSET?"
-        def insetfunct (s):
-            return [int(x) for x in s.split(',')]
 
-        return self._loop_over_chans(cmd, insetfunct)
+class Lakeshore372(VISAInstrument):
+    '''
+    Driver to communicate with LakeShore Model 372 AC Resistance Bridge & Temperature Controller for the BlueFors dilution refrigerator
+    '''
+    _label = 'Lakeshore Model 372'
+    _strip = '\r'
+    _channel_names = {
+        1: '50K',
+        2: '4K',
+        3: 'Magnet',
+        5: 'Still',
+        6: 'MXC',
+        7: 'User1',
+        8: 'User2'
+    }
 
-    def setchsettings(self, chsettings):
+    def __init__(self, host = '192.168.100.143', port=7777):
+        self._init_visa('TCPIP::%s::%i::SOCKET' %(host, port), termination='\n')
+
+        # Make channel objects
+        for c, n in self._channel_names.items():
+            setattr(self, 'chan%i' %c, LakeshoreChannel(self._visa_handle, c, n))
+
+        # Set properties to get/set all params
+        params = ['P', 'R', 'T', 'status', 'scanned', *self.chan1._insets]
+        for p in params:
+            setattr(self, '_' + p, {}) # empty dictionary for later
+            setattr(Lakeshore372, p,
+                property(fget = eval("lambda self: self._get_param('%s')" %p),
+                    fset = eval('lambda self, v: self._set_param("%s", v)' %p)
+                )
+            )
+
+    def __getstate__(self):
+        self._save_dict = {
+            'chan%i' %i: getattr(self, 'chan%i' %i)
+            for i in self._channel_names.keys()
+        }
+
+        return self._save_dict
+
+
+    def _get_param(self, param):
         '''
-        sets all channel settings
-
-        arguments
-            chsettings: (dict) {1: [ <off/on>,<dwell>,<pause>,
-                                    <curve number>,<tempco> ],
-                                    ... 
-                               }
-        returns
-            none
+        Get a parameter from all channels, returned as a dictionary.
         '''
-        for i in chsettings.keys():
-            self.set_channel(i, *chsettings[i])
+        setattr(self, '_' + param,
+            {c: getattr(getattr(self, 'chan%i' %c), param)
+                for c in self._channel_names}
+        )
+        return getattr(self, '_' + param)
 
-
-    def disable_others(self,channel):
+    def _set_param(self, param, d):
         '''
-        Disables all other channels except given channel
-        
-        arguments
-            channel: (int) channel number to enable
-
-        returns
-            none
+        Set the same parameter for all channels with a dictionary
         '''
-        for i in self._channel_names.keys():
-            if (i == channel):
-                continue;
-            chsetting = self.get_channel(i)
-            self.set_channel(i,0,
-                                chsetting[1],
-                                chsetting[2],
-                                chsetting[3],
-                                chsetting[4])
+        for c in d.keys():
+            getattr(self, '_' + param)[c] = d[c]
+            setattr(getattr(self, 'chan%i' %c), param, d[c])
+
+
     def enable_all(self):
         '''
-        Enable all channels
-
-        arguements
-            none
-
-        returns
-            none
+        Enable all channels.
         '''
-        for i in self._channel_names.keys():
-            chsetting = self.get_channel(i)
-            self.set_channel(i,1,
-                                chsetting[1],
-                                chsetting[2],
-                                chsetting[3],
-                                chsetting[4])
-    def disable_ch(self, channel):
-        self._offon_ch(channel, 0)
-
-    def enable_ch(self, channel):
-        self._offon_ch(channel, 1)
-
-    def _offon_ch(self, channel, offon):
-        a = self.get_channel(channel);
-        a[0] = offon;
-        self.set_channel(channel, *a)
+        enabled_now = self.enabled
+        self.enabled = {c: True for c in self._channel_names.keys()
+                if enabled_now[c] is False # to speed things up
+            }
 
 
-
-    def set_channel(self, channel, 
-                   offon=1, dwell=30, pause=10, curvenumber=0, tempco=1):
+    def enable_only(self, channel):
         '''
-        Sets channel parameters
+        Enable only the designated channel.
+        All other channels will be disabled.
 
-        arguments:
-            channel    : channel number, 1-16, 0 is all channels
-            offon      : channel is disabled or enabled.  0=disabled, 1=enabled
-            dwell      : autoscan dwell time, 1-200s
-            pause      : pause time, 3-200 s
-            curvenumber: which curve, 0=nocurve, 1-59 standard/user curves
-            tempco     : temperature coefficient for temperature control if no
-                         curve is selected: 1=negative, 2=positive
+        channel: the channel number to be enabled.
         '''
-        self.write("INSET {0},{1},{2},{3},{4},{5}".format(
-                    channel, offon, dwell, pause, curvenumber, tempco))
+        d = {c: False for c in self._channel_names.keys()}
+        d[channel] = True
+        self.enabled = d
 
-    def get_channel(self, channel):
+
+    def scan(self, channel, autoscan=True):
         '''
-        Gets the channel parameter
+        Start scanning a particular channel.
 
-        arguments:
-            channel: int, channel number
-
-        returns:
-            [ <off/on>,<dwell>,<pause>,<curve number>,<tempco> ]
-
-            see set_channel for details
+        channel: channel number to scan
+        autoscan: whether to automatically loop through channels
         '''
-        s = self.ask("INSET? {0}".format(channel))
-        a = [int(x) for x in s.split(',')]
-        return a
-
-
-    def _loop_over_chans(self, cmd, conversion_type):
-        d = {}
-        for chan in self._channel_names.keys():
-            d[chan] = conversion_type(self.ask('%s %i' %(cmd, chan)))
-        return d
+        getattr(self, 'chan%i' %self._num).scan(autoscan)
