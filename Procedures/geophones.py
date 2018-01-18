d501a1cc33ea13a1c79a4cb9ab4f27ec28fbf233
David Low: Manchot <dhl88@cornell.edu>
2018-01-18 18:10:09 -0500

    [dhl88] geophones, ready for test

--- Procedures/geophones.py
+++ Procedures/geophones.py
@@ -15,7 +15,7 @@ class Geophone_sr5113(Measurement):
                  measure_freq=256000, 
                  averages=30,
                  preamp_diff_mode=False,
-                 conversion=.32 # V/ (cm/s)
+                 conversion=31.5 # V/ (m/s)
                  ):
     '''
     '''
@@ -26,6 +26,14 @@ class Geophone_sr5113(Measurement):
         self.averages = averages
         self.preamp_diff_mode = preamp_diff_mode
         self.conversion = conversion
+
+        # conversion 
+        self.c_acc = lambda s: np.abs(self.conversion * 2*np.pi*s /
+                                        ((2*np.pi*s)**2 + 18*2*np.pi*s + 760))
+        self.c_vel = lambda s: np.abs(self.conversion * (2*np.pi*s)**2 /
+                                        ((2*np.pi*s)**2 + 18*2*np.pi*s + 760))
+        self.c_pos = lambda s: np.abs(self.conversion * (2*np.pi*s)**3 /
+                                        ((2*np.pi*s)**2 + 18*2*np.pi*s + 760))
 
     def do(self):
         self.daqspectrum = DaqSpectrum(instruments=self.instruments,
@@ -40,12 +48,51 @@ class Geophone_sr5113(Measurement):
                                        )
         self.daqspectrum.run(welch=True)
 
-        self.V = np.mean(self.daqspectrum.V, axis=0)
-        self.velocity = self.V/self.conversion
-        self.t = self.daqspectrum.t
-        self.position = np.trapz(self.average_velocity, x=self.t)
-        self.average_acceleration = np.gradient(self.average_velocity, self.t)
+        self.psd = self.daqspectrum.psd
+        self.f   = self.daqspectrum.f
+
+        geo = Geophone(conversion=self.conversion)
+        [self.acc, self.vel, self.pos] = geo.convert(self.psd, self.f)
+
+        self.plot()
+
+    def setup_plots(self):
+        self.fig, self.ax = plt.subplots(3,1, sharex=True)
+        self.ax = list(self.ax.flatten() )
+
+    def plot(self):
+        self.ax[0].semilogy(self.f, self.pos*1e6)
+        self.ax[1].semilogy(self.f, self.vel*1e6)
+        self.ax[2].semilogy(self.f, self.acc*1e6)
+
+        for a in self.ax:
+            a.set_xlim([0,1000])
+
+        self.ax[0].set_ylabel(r'Position ($\mu$ m)')
+        self.ax[1].set_ylabel(r'Velocity ($\mu$ m/s)')
+        self.ax[2].set_ylabel(r'Acceleration ($\mu$ m/s/s)')
+        
+        self.ax[2].set_xlabel('Frequency (Hz)')
 
 
 
+class Geophone(Object):
+    conversion = 0
+    c_acc = lambda s, G: np.abs(G * 2*np.pi*s /
+                                ((2*np.pi*s)**2 + 18*2*np.pi*s + 760))
+    c_vel = lambda s, G: np.abs(G * (2*np.pi*s)**2 /
+                                ((2*np.pi*s)**2 + 18*2*np.pi*s + 760))
+    c_pos = lambda s, G: np.abs(G * (2*np.pi*s)**3 /
+                                ((2*np.pi*s)**2 + 18*2*np.pi*s + 760))
 
+    def __init__(self, 
+                 conversion=31.5 # V/(m/s)
+                 ):
+        self.conversion = conversion
+
+    def convert(self, psd_V, f):
+        acc = psd_V / self.c_acc(f, self.conversion)
+        vel = psd_V / self.c_vel(f, self.conversion)
+        pos = psd_V / self.c_pos(f, self.conversion)
+
+        return [acc, vel, pos]
