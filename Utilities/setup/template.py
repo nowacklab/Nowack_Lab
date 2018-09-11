806afd329e9b0ea8a53bed770f10328921050887
Brian Schaefer <physinet@gmail.com>
2018-09-11 09:48:12 -0400

    Use template setup file for all computers
    Import all the things. Because I want to.

--- Utilities/setup/template.py
+++ Utilities/setup/template.py
@@ -20,8 +20,9 @@ from Nowack_Lab.Utilities import utilities, save, logging, conversions, anim, co
 from Nowack_Lab.Utilities.plotting import plot_mpl
 from Nowack_Lab.Utilities.say import say
 from Nowack_Lab.Utilities.save import Saver
-from Nowack_Lab.Measurements.spectrum import DaqSpectrum, SQUIDSpectrum
-from Nowack_Lab.Measurements.dctransport import DAQ_IV
+from Nowack_Lab.Measurements.array_tune import BestLockPoint
+from Nowack_Lab.Measurements.spectrum import DaqSpectrum, SQUIDSpectrum, ZurichSpectrum
+from Nowack_Lab.Measurements.dctransport import DAQ_IV, RvsT_Montana_Keithley
 from Nowack_Lab.Measurements.heightsweep import Heightsweep
 from Nowack_Lab.Measurements.measurement import Measurement
 from Nowack_Lab.Measurements.mod2D import Mod2D
@@ -33,8 +34,8 @@ from Nowack_Lab.Measurements.scanplane import Scanplane
 from Nowack_Lab.Measurements.scanspectra import Scanspectra
 from Nowack_Lab.Measurements.squidIV import SquidIV
 from Nowack_Lab.Measurements.touchdown import Touchdown
-from Nowack_Lab.Measurements.transport import RvsVg, RvsTime, RvsT, RvsVg_Vtg, RvsT_RT_to_4K, RvsVg_T
-from Nowack_Lab.Measurements.magnetotransport import RvsB, RvsVg_B, RvsB_BlueFors, RvsVg_B_BlueFors
+from Nowack_Lab.Measurements.transport import RvsVg, RvsTime, RvsT, RvsVg_Vtg, RvsT_RT_to_4K, RvsVg_T, PPMS_cool, RvsT_Montana
+from Nowack_Lab.Measurements.magnetotransport import RvsB, RvsVg_B, RvsB_BlueFors, RvsVg_B_BlueFors, RvsB_Phil, RvsVg_B_Phil
 from Nowack_Lab.Instruments.attocube import Attocube
 from Nowack_Lab.Instruments.keithley import Keithley2400, Keithley2450, KeithleyPPMS
 from Nowack_Lab.Instruments.lakeshore import Lakeshore372
@@ -43,6 +44,7 @@ from Nowack_Lab.Instruments.nidaq import NIDAQ
 from Nowack_Lab.Instruments.piezos import Piezos
 from Nowack_Lab.Instruments.preamp import SR5113
 from Nowack_Lab.Instruments.squidarray import SquidArray
+from Nowack_Lab.Instruments.sr760 import SR760
 from Nowack_Lab.Instruments.lockin import SR830
 from Nowack_Lab.Instruments.ppms import PPMS
 from Nowack_Lab.Instruments.magnet import Magnet, AMI420
