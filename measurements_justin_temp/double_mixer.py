d3f1916fe21257b02f09b0504e1226fdc2a8e03f
cactus <cactus@nowacklab>
2018-10-05 17:00:08 -0400

    start writing temporary measurement code for testing double downconv. circuit

---
+++ measurements_justin_temp/double_mixer.py
@@ -0,0 +1,29 @@
+""" Procedure to take a DC power out vs. bias flux graph for RF SQUID
+This is only a temporary program; should eventually make it actually use
+other classes etc.
+Should implement normal data saving right away"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+import time, os
+from datetime import datetime
+from ..Instruments.nidaq import NIDAQ
+
+class double_mixer_measurement():
+
+    """general operation: steps daq voltage out to
+    step flux bias; at each step, record the
+    voltage from the daq input"""
+
+    def __init__(self, bias_resistance, Ioutmin, Ioutmax, numpoints):
+        self.dq = NIDAQ() # create new daq object
+
+    def do(self):
+        self.dq.ao0.V = 0 # zero daq 0 ouput
+
+        daq_iout_range = range(Ioutmin, Ioutmax, numpoints)
+        daq_vout_range = bias_resistance*daq_iout_range
+
+        arr = numpy.zeros(
+
+        for i in daq_iout_range:
