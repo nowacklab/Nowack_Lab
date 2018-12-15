d25711780fe9b25f635f3342021036600a8b8ccd
phil <nowacklab@gmail.com>
2018-12-15 00:31:02 -0500

    Two point DC transport options
    Either aux port of Zurich or monitor Keithley

--- Measurements/dctransport.py
+++ Measurements/dctransport.py
@@ -233,14 +233,15 @@ class RvsSomething_DC(RvsSomething):
     Preamp may be used before the Zurich input. Specify preamp_gain manually.
     NOTE the preamp_gain parameter only affects the signal received from the Zurich's normal inputs.
 
-    get_aux: if want to record voltage into aux input
+    two_point: 'aux' or 'keithley'. 'aux' will take an average as described above
+    'keithley' will record the keithley voltage output.
     '''
     instrument_list = ['zurich', 'keithleybias']
     something='time'
     something_units = 's'
     legendtitle=None
 
-    def __init__(self, instruments = {}, preamp_gain=1, get_aux=True):
+    def __init__(self, instruments = {}, preamp_gain=1, two_point='aux'):
         Measurement.__init__(self, instruments=instruments)
 
         # Set up empty arrays
@@ -255,7 +256,8 @@ class RvsSomething_DC(RvsSomething):
         setattr(self, self.something, np.array([]))
 
         self.preamp_gain = preamp_gain
-        self.get_aux = get_aux
+        assert two_point in ('keithley', 'aux')
+        self.two_point = two_point
 
     def do(self, *args, **kwargs):
         return RvsSomething.do(self, auto_gain=True, **kwargs)
@@ -289,9 +291,12 @@ class RvsSomething_DC(RvsSomething):
         r = 0
         r2p = 0
         for i in range(num_avg):
-            if self.get_aux:
+            if self.two_point == 'aux':
                 t, V = self.zurich.get_scope_trace(self.zurich.freq_opts[10], input_ch=9) # aux in 2
                 v2p += V.mean()
+                r2p += v2p/self.I[-1]
+            elif self.two_point == 'keithley':
+                v2p += self.keithleybias.V
                 r2p += v2p/self.I[-1]
 
             t, V = self.zurich.get_scope_trace(self.zurich.freq_opts[10])
