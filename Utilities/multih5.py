e0a3f94e8bcb8f0ada7927209238eec40f6cfe2c
Alex Striff <abs299@cornell.edu>
2022-06-20 18:03:59 -0400

    Basic dummy sweep saving and plotting code

--- Utilities/multih5.py
+++ Utilities/multih5.py
@@ -2,12 +2,9 @@ import h5py as h5
 from contextlib import ExitStack
 from Nowack_Lab.Utilities.datasaver import Saver
 
-# Welcome to the hall of black magic. We hope you enjoy your stay.
-
-class File():
+class Files():
     '''
-    Wrapper for h5 and datasaver that allows logical access to several
-    duplicate h5 files in one interface.
+    Convenient multiple-file context manager for saving data files.
     '''
     def __init__(self, name='', *args, **kwargs):
         super().__setattr__('filenames', [d.filename
@@ -19,35 +16,12 @@ class File():
             cm.pop_all()
         super().__setattr__('', files)
 
-    def wrapcallable(xs):
-        if callable(xs[0]):
-            return lambda *args, **kwargs: [x(*args, **kwargs) for x in xs]
-        else:
-            return xs
-
-    def __getattr__(self, attr, *args, **kwargs):
-        attrs = [x.__getattribute__(attr, *args, **kwargs)
-                for x in super().__getattribute__('')]
-        return wrapcallable(attrs)
-
-    def __getitem__(self, item, *args, **kwargs):
-        items = [x.__getitem__(item, *args, **kwargs)
-                for x in super().__getattribute__('')]
-        return wrapcallable(items)
-
-    def __setattr__(self, attr, *args, **kwargs):
-        for x in super().__getattribute__(''):
-            setattr(x, attr, *args, **kwargs)
-
-    def __setitem__(self, item, *args, **kwargs):
-        for x in super().__getattribute__(''):
-            x.__setitem__(item, *args, **kwargs)
-
     def __enter__(self):
-        return self
+        return super().__getattribute__('')
 
     def __exit__(self, type, value, traceback):
         self.__getattribute__('close')()
+        return self.filenames
 
     def close(self):
         for x in super().__getattribute__(''):
